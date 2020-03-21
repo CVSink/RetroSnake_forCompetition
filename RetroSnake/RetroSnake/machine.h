d26be7884e0641d6947b9d6f459850a97475baed
CVSink <53992361+CVSink@users.noreply.github.com>
2020-03-21 23:08:58 +0800

    Dynamic_Programing_method 1st
    Dynamic_Programing_method 1st commit.

--- RetroSnake/RetroSnake/machine.h
+++ RetroSnake/RetroSnake/machine.h
@@ -74,10 +74,75 @@ bool check(point snake[5][100], int len[5], int t, GamePanel gp, int direction)
 	return true;
 }
 
+void initial(int map[1200][1200], GamePanel& gp,point snake[5][100]) {
+	for (int i = 0; i < gp.n * gp.m / 2; ++i) {
+		for (int j = 0; j < gp.n * gp.m / 2; ++j) {
+			if (i == j) map[i][j] = 0;
+			map[i][j] = -1;
+			if (i / gp.n != 0 && i / gp.n == j / gp.n && i % gp.n == j + 1 && gp.panel[2 * (i / gp.n + 1) - 2][i % gp.n + 1] == 0) {	//下方
+				map[i][j] = 1;
+			}
+			else if (i / gp.n != 0 && i / gp.n == j / gp.n && i % gp.n == j + 1 && gp.panel[2 * (i / gp.n + 1) - 2][i % gp.n + 1] == 2) {	//下方
+				map[i][j] = -1;
+			}
+			if (i / gp.n != gp.m / 2 && i / gp.n == j / gp.n && i % gp.n == j - 1 && gp.panel[2 * (i / gp.n + 1) + 2][i % gp.n + 1] == 0) {	//上方
+				map[i][j] = 1;
+			}
+			else if (i / gp.n != gp.m / 2 && i / gp.n == j / gp.n && i % gp.n == j - 1 && gp.panel[2 * (i / gp.n + 1) - 2][i % gp.n + 1] == 2) {	//上方
+				map[i][j] = -1;
+			}
+			if (i % gp.n != 0 && i / gp.n - 1 == j / gp.n && i % gp.n == j && gp.panel[2 * (i / gp.n + 1)][i % gp.n] == 0) {	//左边
+				map[i][j] = 1;
+			}
+			else if (i % gp.n != 0 && i / gp.n - 1 == j / gp.n && i % gp.n == j && gp.panel[2 * (i / gp.n + 1) - 2][i % gp.n + 1] == 2) {	//左方
+				map[i][j] = -1;
+			}
+			if (i % gp.n != gp.n && i / gp.n + 1 == j / gp.n && i % gp.n == j && gp.panel[2 * (i / gp.n + 1)][i % gp.n + 2] == 0) {	//右边
+				map[i][j] = 1;
+			}
+			else if (i % gp.n != gp.n && i / gp.n + 1 == j / gp.n && i % gp.n == j && gp.panel[2 * (i / gp.n + 1) - 2][i % gp.n + 1] == 2) {	//右方
+				map[i][j] = -1;
+			}
+
+		}
+	}
+	return;
+}
+
+void AllLength(int n,int map[1200][1200]) {
+	int A[1200][1200];
+	int path[1200][1200];
+	int max=9999;
+	for (int i = 0; i < n; ++i) {
+		for (int j = 0; j < n; ++j) {
+			A[i][j] = map[i][j];
+			if (i != j && A[i][j] < max) {
+				path[i][j] = i;
+			}
+			else {
+				path[i][j] = -1;
+			}
+		}
+	}
+	for (int k = 0; k < n; ++k) {
+		for (int i = 0; i < n; ++i) {
+			if (i != k) {
+				for (int j = 0; j < n; ++j) {
+					if (j != k && j != i && A[i][k] < max && A[k][j] < max && A[i][j] + A[k][j] < A[i][j]) {
+						A[i][j] = A[i][k] + A[k][j];
+						path[i][j] = path[k][j];
+					}
+				}
+			}
+		}
+	}
+}
 
 int machine_move(point snake[5][100], int len[5], int direct[5], int t, GamePanel gp)
 {
-	int map[2200][2200];
+	int map[1200][1200];
+	initial(map, gp, snake);
+	AllLength(gp.n * gp.m / 2, map);
 	//将你的check函数体放在此处，并删去下面示例代码
 	if (t == 0) return 2;
 	else return 1;
